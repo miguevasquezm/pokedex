ef3d9cb71e27628b58ca9b4ca866fad848bddc32
miguevasquezm <147664660+miguevasquezm@users.noreply.github.com>
2023-12-06 14:36:43 -0500

    Add files via upload

---
+++ array.js
@@ -0,0 +1,12 @@
+export var normales = [
+"https://files.cults3d.com/uploaders/16165612/illustration-file/0c2a6bf7-7bca-4819-ae07-a22f74ca7226/Snorlax04.jpg",
+"https://files.cults3d.com/uploaders/17560495/illustration-file/3304afcc-fd0a-4f03-9f3e-5d787029d653/eevee.jpg",
+"https://files.cults3d.com/uploaders/13957545/illustration-file/463ec71d-ce07-4249-a6d6-fee455f36015/22.jpg"
+]
+
+export var descripcion = [
+"es de gran tamaño y con pelaje adiamantado a poco pulir, se caracteriza en el anime y los videojuegos por limitarse a comer y dormir",
+"es un Pokémon de tipo normal introducido en la primera generación. Se caracteriza por ser el Pokémon con más evoluciones posibles, con ocho",
+" es una especie de Pokémon de Nintendo y de la franquicia de Pokémon de Game Freak"
+]
+
---
+++ index.js
@@ -0,0 +1,19 @@
+import {normales} from './array.js'
+const ade = document.getElementById("btn")
+let poke = document.getElementById("snor")
+
+for (let i = 0; i < normales.length; i++) {
+    const imagen = normales[i];  
+
+btn.addEventListener("click",function(){
+    event.preventDefault()
+    poke.setAttribute('src' , imagen )
+})
+}
+
+document.getElementById ("snor")
+
+var back= document.getElementById("back")
+var next= document.getElementById("next")
+contador=0;
+
